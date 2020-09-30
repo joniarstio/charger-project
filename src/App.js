ed24ccf80f0836d867c69c2b4f3f10beaf6c1e5e
Venla Jokikokko <t9jove00@students.oamk.fi>
2020-09-30 16:35:10 +0300

    WIP

--- src/App.js
+++ src/App.js
@@ -6,27 +6,37 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <div className="App-header">
-            <h1>Header</h1>
+    <body>
+      {/* sidebar and nav */}
+      <div class="App-header">
+          <h1 class="main">Header</h1>
             <h1>Login</h1>
             <h1>Home</h1>
         </div>
       <div className="App-search">
       </div>
-        <div className="App-body">
-          <div className="App-nav">
-              <ul>
-               <a>Something</a>
-               <a>Something</a>
+        <div className="App-nav">
+          <ul>
+            <li>
+              <a>Something</a>
+            </li>
+            <li>
+              <a>Something</a>
+            </li>
+            <li>
               <a>something</a>
-              </ul>
-          </div>
+            </li>
+          </ul>
         </div>
+        <div className="App-body">
+          Kartta
+        </div>
+
         <div className="App-footer">
             <h2>footer</h2>
     </div>
-    </div>
+
+  </body>
   );
  }
 }
