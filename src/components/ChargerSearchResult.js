2e534f8352d9ff994ccf45f0ad520dc719afb1c5
Venla Jokikokko <t9jove00@students.oamk.fi>
2020-10-02 11:28:26 +0300

    WIP

--- src/components/ChargerSearchResult.js
+++ src/components/ChargerSearchResult.js
@@ -6,7 +6,6 @@ export default function ChargerSearchResult(props) {
             <div>
                 <div>{props.name}</div>
                 <div>{props.location}</div>
-                <div>{props.connectorType}</div>
                 <div>{props.price}</div>
             </div>
         </div>
