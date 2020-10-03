1db752ddfb386a80944172a3457f4e7cdc58a500
Venla Jokikokko <t9jove00@students.oamk.fi>
2020-10-03 11:41:58 +0300

    WIP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 import SearchView from './components/ChargerSearchView';
-import data from './data.json'
+
 
 
 class App extends React.Component {
@@ -9,9 +10,18 @@ class App extends React.Component {
   {
     super(props);
     this.state = {
-      chargers: data.chargers,
+      charger: [],
       chargerSearchString: ""
     }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/chargers')
+      .then(response => {
+        console.log(response);
+        this.setState({ charger: response.data.chargers})
+      })
+      .catch((err) => console.log(err));
   }
 
   onSearchChange = (event) => {
@@ -22,20 +32,20 @@ class App extends React.Component {
   render() 
   {
     return (
-      <main>
+    <main>
       {/* sidebar and nav */}
       <header>
-      <div className="App-header">
-        <h1 className="main">Charge</h1>
-        <h1 className="main-second">Up</h1>
-      </div>
-      <div className="App-search">
-        <input type="text" onChange={this.onSearchChange} value={this.state.chargerSearchString}/>
-      </div>
-      <div className="App-regAndlog">
-        <h2>Login</h2>
-        <h2>Sing Up</h2>
-      </div>
+        <div className="App-header">
+          <h1 className="main">Charge</h1>
+          <h1 className="main-second">Up</h1>
+        </div>
+        <div className="App-search">
+          <input type="text" onChange={this.onSearchChange} value={this.state.chargerSearchString}/>
+        </div>
+        <div className="App-regAndlog">
+          <h2>Login</h2>
+          <h2>Sing Up</h2>
+        </div>
         <div className="App-nav">
           <ul>
             <li>
@@ -46,13 +56,13 @@ class App extends React.Component {
             </li>
           </ul>
         </div>
-        </header>
-        <div className="App-view">
-          <div className="Map">
-            <SearchView chargers={this.state.chargers.filter((charger) => charger.name.includes(this.state.chargerSearchString))} />
-          </div>
+      </header>
+      <div className="App-view">
+        <div className="Map">
+          <SearchView chargers={this.state.charger.filter((charger) => charger.name.includes(this.state.chargerSearchString))} />
         </div>
-      </main>
+      </div>
+    </main>
   );
  }
 }
--- src/components/ChargerSearchResult.js
+++ src/components/ChargerSearchResult.js
@@ -6,6 +6,7 @@ export default function ChargerSearchResult(props) {
             <div>
                 <div>{props.name}</div>
                 <div>{props.location}</div>
+                <div>{props.chargerType}</div>
                 <div>{props.price}</div>
             </div>
         </div>
