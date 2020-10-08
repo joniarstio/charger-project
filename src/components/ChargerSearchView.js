75106e304808293ca0a3818023827313f9df81a1
Joni Arstio <joni.arstio@gmail.com>
2020-10-08 15:43:17 +0300

    amazon server setup

---
+++ src/components/ChargerSearchView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SearchResult from './ChargerSearchResult';
+
+export default function ChargerSearchView(props) {
+
+    return (
+        <div>
+            <div>
+                {
+                    props.chargers.map(charger => <SearchResult key={charger.id} {...charger} /> )
+                }
+            </div>
+        </div>
+    )
+}
