c1efc050f32dd0a4e40a8db5006ea7bdd966a65c
Venla Jokikokko <t9jove00@students.oamk.fi>
2020-10-14 08:11:17 +0300

    update

--- src/components/UserPool.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
-
-const poolData = {
-  UserPoolId: 'us-east-2_ZPdnevPkt',
-  ClientId: '57qodd5q9stcspi9v5lla0vs2'
-};
-
-export default new CognitoUserPool(poolData);
